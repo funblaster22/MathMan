d1e691b6d00ab259f6d2a4836e1b221b2bef1952
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-12-11 15:21:55 -0500

    Encapsulate File component

---
+++ src/routes/[...path=notint]/File.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import {db, type File} from "$lib/db";
+  import type {Writable} from "svelte/store";
+  import path from "path";
+  import {base} from "$app/paths";
+  import {page} from "$app/stores";
+
+  export let file: File;
+  export let deleteEnabled: Writable<boolean>;
+
+  const href = path.join("/", base, $page.params.path, file.id!.toString());
+
+  /** Retrieve date of most recent attempt in given `file` */
+  const recentestAttempt = (function(file: File) {
+    if (file.attempts.length === 0)
+      return {date: new Date(0), duration: 0};
+    return file.attempts.reduce((prev, current) => {
+      return current.date > prev.date ? current : prev;
+    });
+  })(file);
+
+  /**
+   * Report a relative date in the most logical unit (days until 10, weeks until 16, months until 12, years).
+   * @param date Time since now to report
+   * @param terse if true, use units d, w, m, or y. Otherwise, days, weeks, months, years (respectively)
+   * @returns formatted string. Ex: -5d (5 days ago), -6
+   */
+  function timeAgo(date: Date, terse = true): string {
+    const now = new Date();
+    const daysAgo = (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+    if (daysAgo <= 10) {
+      return Math.floor(daysAgo) + (terse ? "d" : " days");
+    }
+    const weeksAgo = daysAgo / 7;
+    if (weeksAgo <= 16) {
+      return Math.floor(weeksAgo) + (terse ? "w" : " weeks");
+    }
+    const yearsAgo = now.getFullYear() - date.getFullYear();
+    const monthsAgo = yearsAgo * 12 + (now.getMonth() - date.getMonth());
+    if (monthsAgo < 12) {
+      return monthsAgo + (terse ? "m" : " months");
+    }
+    return Math.floor(monthsAgo / 12) + (terse ? "y" : " years");
+  }
+
+  /** Show attempts or delete file, depending on state */
+  function auxButtonClicked(id: number) {
+    if ($deleteEnabled) {
+      db.files.delete(id);
+    }
+    // TODO: show children
+  }
+</script>
+
+<div class="text-center w-[6rem] mr-[11px]">
+    <div class="relative">
+        <a {href}>
+            <div class="w-full aspect-square border m-auto grid grid-cols-2 grid-rows-3">
+                <div class="m-auto" title="Time since last attempt ({timeAgo(recentestAttempt.date, false)})">-{timeAgo(recentestAttempt.date)}</div>
+                <div class="m-auto" title="Duration of last attempt ({recentestAttempt.duration} minutes)">⏱️ {recentestAttempt.duration}</div>
+                <div class="m-auto" title="Number of attempts">✏️ {file.attempts.length}</div><div></div>
+                <div class="m-auto" title="Number of unresolved questions">❔ {file.questions}</div>
+                <div class="m-auto" title="Weighted average of incorrect problems">❌ {file.mistakes}</div>
+            </div>
+        </a>
+        <button class="attempt-dropdown" title="Show attempts" on:click={auxButtonClicked.bind(this, file.id)}>{$deleteEnabled ? "🗑️" : "🔻"}</button>
+    </div>
+    <a {href}><div>{file.flagged ? (new Date().getMonth() === 5 ? "🏳️‍🌈 " : "🚩 ") : ""}{file.name}</div></a>
+</div>
+
+<style>
+    .attempt-dropdown {
+        position: absolute;
+        right: -11px;
+        top: 50%;
+        border: 1px solid;
+        border-radius: 1rem;
+        background: hsl(var(--b1) / var(--tw-bg-opacity, 1));
+        transform: translateY(-50%);
+    }
+</style>
--- src/routes/[...path=notint]/FileViewer.svelte
+++ src/routes/[...path=notint]/FileViewer.svelte
@@ -5,6 +5,7 @@
   import path from "path";
   import {base} from "$app/paths";
   import type {Writable} from "svelte/store";
+  import FileComponent from "./File.svelte";
 
   export let fileStruct: FileStructure;
   export let deleteEnabled: Writable<boolean>;
@@ -21,48 +22,6 @@
     }
   }
 
-  /** Retrieve date of most recent attempt in given `file` */
-  function recentestAttempt(file: File) {
-    if (file.attempts.length === 0)
-      return {date: new Date(0), duration: 0};
-    return file.attempts.reduce((prev, current) => {
-        return current.date > prev.date ? current : prev;
-    });
-  }
-
-  /**
-   * Report a relative date in the most logical unit (days until 10, weeks until 16, months until 12, years).
-   * @param date Time since now to report
-   * @param terse if true, use units d, w, m, or y. Otherwise, days, weeks, months, years (respectively)
-   * @returns formatted string. Ex: -5d (5 days ago), -6
-   */
-  function timeAgo(date: Date, terse = true): string {
-    const now = new Date();
-    const daysAgo = (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
-    if (daysAgo <= 10) {
-      return Math.floor(daysAgo) + (terse ? "d" : " days");
-    }
-    const weeksAgo = daysAgo / 7;
-    if (weeksAgo <= 16) {
-      return Math.floor(weeksAgo) + (terse ? "w" : " weeks");
-    }
-    const yearsAgo = now.getFullYear() - date.getFullYear();
-    const monthsAgo = yearsAgo * 12 + (now.getMonth() - date.getMonth());
-    if (monthsAgo < 12) {
-      return monthsAgo + (terse ? "m" : " months");
-    }
-    return Math.floor(monthsAgo / 12) + (terse ? "y" : " years");
-  }
-
-  /** Show attempts or delete file, depending on state */
-  function auxButtonClicked(id: number) {
-    if ($deleteEnabled) {
-      db.files.delete(id);
-    }
-    // TODO: show children
-  }
-
-  // Variable initializer is not redundant (TODO: check if this is a SSR quirk)
   let files: Observable<File[]>;
   // Ignore, it is the right type
   $: files = liveQuery(() =>
@@ -79,39 +38,10 @@
         </a>
     {/each}
     {#each ($files ?? []) as file (file.id)}
-        <!-- TODO: replace w/ file!.id once Svelte supports typescript in markup https://github.com/sveltejs/svelte/issues/4701 -->
-        {@const href = path.join("/", base, $page.params.path, file.id.toString())}
-        {@const myRecentestAttempt = recentestAttempt(file)}
-        <div class="text-center w-[6rem] mr-[11px]">
-            <div class="relative">
-                <a {href}>
-                    <div class="w-full aspect-square border m-auto grid grid-cols-2 grid-rows-3">
-                        <div class="m-auto" title="Time since last attempt ({timeAgo(myRecentestAttempt.date, false)})">-{timeAgo(myRecentestAttempt.date)}</div>
-                        <div class="m-auto" title="Duration of last attempt ({myRecentestAttempt.duration} minutes)">⏱️ {myRecentestAttempt.duration}</div>
-                        <div class="m-auto" title="Number of attempts">✏️ {file.attempts.length}</div><div></div>
-                        <div class="m-auto" title="Number of unresolved questions">❔ {file.questions}</div>
-                        <div class="m-auto" title="Weighted average of incorrect problems">❌ {file.mistakes}</div>
-                    </div>
-                </a>
-                <button class="attempt-dropdown" title="Show attempts" on:click={auxButtonClicked.bind(this, file.id)}>{$deleteEnabled ? "🗑️" : "🔻"}</button>
-            </div>
-            <a {href}><div>{file.flagged ? (new Date().getMonth() === 5 ? "🏳️‍🌈 " : "🚩 ") : ""}{file.name}</div></a>
-        </div>
+        <FileComponent {file} {deleteEnabled} />
     {/each}
     <!-- Padding to make sure `justify-evenly` places extra items left-aligned -->
     {#each Array(10) as _}
         <div class="w-[6rem] mr-[11px]"></div>
     {/each}
 </div>
-
-<style>
-    .attempt-dropdown {
-        position: absolute;
-        right: -11px;
-        top: 50%;
-        border: 1px solid;
-        border-radius: 1rem;
-        background: hsl(var(--b1) / var(--tw-bg-opacity, 1));
-        transform: translateY(-50%);
-    }
-</style>
