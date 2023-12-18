190f79ea176762a068ad833f588e5bb8b9dcfa40
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-12-18 18:26:12 +0000

    Deploying to gh-pages from @ funblaster22/MathMan@97bf6718d8b2904e16bde46255ed711f13d355ad 🚀

---
+++ _app/immutable/nodes/3.a5917d38.js
@@ -0,0 +1,1 @@
+import{L as n}from"../chunks/layout.9ef42956.js";const o=({params:e})=>({problemId:Number.parseInt(e.problemId)}),r=Object.freeze(Object.defineProperty({__proto__:null,load:o},Symbol.toStringTag,{value:"Module"}));export{n as component,r as universal};
