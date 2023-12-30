cd04470675ce641cd5147112c75f272a0bba8a25
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-12-30 00:29:52 +0000

    PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,34 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script lang="ts">
+  import "../app.postcss";
+  import { pwaInfo } from 'virtual:pwa-info';
+  import {onMount} from "svelte";
+
+  // Adapted from https://vite-pwa-org.netlify.app/frameworks/sveltekit.html
+  onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register');
+      registerSW({
+        immediate: true,
+        onRegistered(r: ServiceWorkerRegistration) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log("SW Registered:", r);
+        },
+        onRegisterError(error: any) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
+  })
+
+  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+</script>
+
+<svelte:head>
+    {@html webManifestLink}
+</svelte:head>
+
+<slot></slot>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		// TODO: maybe I have to use injectManifest to handle SPA
+		SvelteKitPWA({
+			manifest: {
+				icons: [
+					{
+						src: '/favicon.png',
+						sizes: '128x128',
+						type: 'image/png',
+					},
+					{
+						src: '/favicon2.png',
+						sizes: '256x256',
+						type: 'image/png',
+					},
+				],
+			},
+		})
+	],
 	resolve: {
 		alias: {
 			path: "path-browserify",
