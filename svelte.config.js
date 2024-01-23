83786f045be867bdcce8599d3de716af1a64fca3
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-01-23 13:12:29 -0500

    Prerender index page

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({fallback: "404.html"}),
+    prerender: {
+      entries: ["/"],
+    },
     paths: {
       base: process.argv.includes('dev') ? '/BASE_PATH' : process.env.BASE_PATH
     }
