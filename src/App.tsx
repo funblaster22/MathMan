7c871399dc3347d82920f2dcd99acc89e4aad11e
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-02-13 09:23:06 -0500

    Test excalidraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 import {Excalidraw} from "@excalidraw/excalidraw";
+import {useState} from "react";
+import {ExcalidrawImperativeAPI} from "@excalidraw/excalidraw/types/types";
 
 function App() {
+  const [api, setApi] = useState<ExcalidrawImperativeAPI>();
 
   return (
-    <Excalidraw />
+    <Excalidraw
+      zenModeEnabled={true}
+      gridModeEnabled={true}
+      excalidrawAPI={setApi}
+    >
+    </Excalidraw>
   )
 }
 
