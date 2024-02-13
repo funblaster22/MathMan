055a898a2dea2c7123dff868bca903cbea515115
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-02-13 14:09:39 -0500

    Switch to tldraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import './App.css'
-import {Excalidraw} from "@excalidraw/excalidraw";
-import {useState} from "react";
-import {ExcalidrawImperativeAPI} from "@excalidraw/excalidraw/types/types";
+import {Tldraw} from "@tldraw/tldraw";
 
 function App() {
-  const [api, setApi] = useState<ExcalidrawImperativeAPI>();
-
   return (
-    <Excalidraw
-      zenModeEnabled={true}
-      gridModeEnabled={true}
-      excalidrawAPI={setApi}
-    >
-    </Excalidraw>
+    <Tldraw />
   )
 }
 
