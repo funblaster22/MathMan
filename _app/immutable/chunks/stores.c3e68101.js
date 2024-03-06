b9753b446ba3cb7b3342142e81534eb272c7a575
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-03-06 05:03:14 +0000

    Deploying to gh-pages from @ funblaster22/MathMan@d96830637df70e61b4784981b878a2bd21ed2796 🚀

---
+++ _app/immutable/chunks/stores.c3e68101.js
@@ -0,0 +1,2 @@
+import{d as e}from"./singletons.635f2ddb.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=stores.c3e68101.js.map
